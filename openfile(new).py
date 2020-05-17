43b8aff3410f85ef95c942b9f8c311b03ff92716
gao <63588920+floodlits@users.noreply.github.com>
2020-05-17 11:40:51 +0800

    Update openfile(new).py

--- openfile(new).py
+++ openfile(new).py
@@ -1,17 +1,24 @@
 def openFile(fileName):
+    """ Open the file """
     rows = list()
-    newlist = list()
-    csvfile = open(fileName,'r', encoding='UTF-8')
+    new_list = list()
+    csvfile = open(fileName,'r', errors = "ignore")
+    n = 1
     for strline in csvfile.readlines():
-        strline = strline.replace('",','*')
-        strline = strline.replace(',"','*')
-        listline = strline.strip().split('*')
-        for index, element in enumerate(listline):
-            element = element.strip('"')
-            newlist.append(element)
-        newlist[2],newlist[-1] = newlist[-1],newlist[2]
-        rows.append(newlist)
-        newlist = []
+        if n == 1:
+            n = 0
+            continue
+        else:
+            strline = strline.replace('",','*')
+            strline = strline.replace(',"','*')
+            listline = strline.strip().split('*')
+            for index, element in enumerate(listline):
+                element = element.strip('"')
+                new_list.append(element)
+        # Exchange the "Rating" and "Android.Ver" to put the label in the last position
+        new_list[2],new_list[-1] = new_list[-1],new_list[2]
+        rows.append(new_list)
+        new_list = []
+    # Pop out the header
     rows.pop(0)
-    print(rows[0])
     return rows
